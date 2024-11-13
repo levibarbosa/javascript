970473be7902af8206c0493c0384d9de0f5d6f2b
levibarbosa <levibarbos4@hotmail.com>
2024-11-12 23:18:49 -0300

    basic task 01

---
+++ basic/tasks/task-01.js
@@ -0,0 +1,4 @@
+// display the following sentence in the console:
+// My name is "your name". I am learning JavaScript at 10 in the morning.
+
+console.log(`My name is "Levi Barbosa". I am learning JavaScript at`, 10, ` in the morning.`);
