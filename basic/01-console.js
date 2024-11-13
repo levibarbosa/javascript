042ae5cc68b91b0e85b2a440ac16cfef754b49b0
levibarbosa <levibarbos4@hotmail.com>
2024-11-12 23:09:28 -0300

    console

---
+++ basic/01-console.js
@@ -0,0 +1,7 @@
+// log is a method of the "Console" class used to display information in the terminal
+
+console.log("Hello, world!"); // displaying a string in the terminal
+console.log(42); // displaying an int in the terminal
+console.log(42.0); // displaying a float in the terminal
+
+// note: int and float are simply "number" in JavaScript
