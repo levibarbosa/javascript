e8d0b3178e49e1e781767fb17cd20581df21501c
levibarbosa <levibarbos4@hotmail.com>
2024-11-16 23:52:07 -0300

    comments

---
+++ basic/02-comments.js
@@ -0,0 +1,7 @@
+// console.log(`Esta linha é um comentário.`);
+console.log(`Esta é a segunda linha.`);
+/*
+console.log(`Este bloco é um comentário.`);
+console.log(`Este bloco é um comentário.`);
+console.log(`Este bloco é um comentário.`);
+*/
