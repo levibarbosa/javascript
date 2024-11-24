65d7714c01557218648ac86a69f1d9a9265d7329
levibarbosa <levibarbos4@hotmail.com>
2024-11-24 16:00:52 -0300

    task 02

---
+++ basic/tasks/task-02.js
@@ -0,0 +1,27 @@
+/* 
+Exercício: Criando um Perfil de Usuário.
+
+Objetivo: Criar um perfil de usuário simples, armazenando informações básicas e exibindo-as no console.
+
+Instruções:
+
+1 - Crie as seguintes constantes:
+
+    nome: Para armazenar o nome do usuário.
+    idade: Para armazenar a idade do usuário.
+    cidade: Para armazenar a cidade onde o usuário mora.
+
+2 - Crie uma variável let para armazenar uma frase com as informações do usuário.
+
+3 - Utilize console.log para exibir a frase.
+
+*/
+
+const nome = "Levi Barbosa Rodrigues Fonseca";
+const idade = 25;
+const cidade = "Palmas";
+
+let frase = `Meu nome é ${nome}, tenho ${idade} anos e moro na cidade de ${cidade}.`;
+
+console.log(frase);
+
