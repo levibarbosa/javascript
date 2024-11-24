fb34ce5ba30cbf127cbb30e2d7b763af8cd52fa7
levibarbosa <levibarbos4@hotmail.com>
2024-11-24 20:45:52 -0300

    task 03

---
+++ basic/tasks/task-03.js
@@ -0,0 +1,24 @@
+/* 
+
+Você tem três variáveis: a, b e c, com os valores iniciais 'a', 'b' e 'c', respectivamente.
+Crie um algoritmo que troque os valores dessas variáveis de forma que a tenha o valor de b,
+b tenha o valor de c e c tenha o valor de a.
+
+*/
+
+// Valores iniciais
+let a = 'a';
+let b = 'b';
+let c = 'c';
+
+// Valores finais esperados:
+// a = 'b'
+// b = 'c'
+// c = 'a'
+
+// resposta:
+[a, b, c] = [b, c, a];
+
+console.log(a);
+console.log(b);
+console.log(c);
