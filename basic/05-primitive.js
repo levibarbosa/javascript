1a466c6673e002d0d14abdedeb85ca978657106e
levibarbosa <levibarbos4@hotmail.com>
2024-11-24 19:47:37 -0300

    primitive

---
+++ basic/05-primitive.js
@@ -0,0 +1,29 @@
+// Tipos primitivos em JavaScript
+
+// String (texto)
+let texto = "Olá, mundo!";
+console.log(texto);
+
+// Number (número)
+let numeroInteiro = 42;
+let numeroDecimal = 3.14;
+console.log(numeroInteiro);
+console.log(numeroDecimal);
+
+// Boolean (lógico)
+let verdadeiro = true;
+let falso = false;
+console.log(verdadeiro);
+console.log(falso);
+
+// null (nulo)
+let nulo = null;
+console.log(nulo);
+
+// undefined (indefinido)
+let indefinido; // Variável declarada, mas sem valor atribuído
+console.log(indefinido);
+
+// Symbol (símbolo)
+let simbolo = Symbol("meu simbolo");
+console.log(simbolo);
