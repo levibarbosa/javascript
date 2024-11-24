8569972be206f477cd722ed51b979eeba8c24a63
levibarbosa <levibarbos4@hotmail.com>
2024-11-24 19:52:37 -0300

    arithmetic

---
+++ basic/06-arithmetic.js
@@ -0,0 +1,27 @@
+let num1 = 10;
+let num2 = 5;
+
+// Operações aritméticas
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+let modulo = num1 % num2;
+
+// Atribuições com operações
+num1 += 3; // num1 agora é 13
+num2 *= 2; // num2 agora é 10
+
+// Incremento e decremento
+let contador = 0;
+contador++; // contador agora é 1
+contador--; // contador volta a ser 0
+
+console.log("Soma:", soma);
+console.log("Subtração:", subtracao);
+console.log("Multiplicação:", multiplicacao);
+console.log("Divisão:", divisao);
+console.log("Módulo:", modulo);
+console.log("Valor final de num1:", num1);
+console.log("Valor final de num2:", num2);
+console.log("Valor final de contador:", contador);
